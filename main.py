1350a8d56c949776ec7ea770af72862fbddd3856
Markus Rößler <barytdruck@freenet.de>
2024-12-08 08:28:03 +0100

    update

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ else:
 print(f"Verwendetes Gerät: {device}")
 
 # Laden der Modelle deutsch / englisch und umgekehrt
-de_to_en = pipeline("translation", model="Helsinki-NLP/opus-mt-de-en", device=device)
-en_to_de = pipeline("translation", model="Helsinki-NLP/opus-mt-en-de", device=device)
+de_to_en = pipeline("translation", model="Helsinki-NLP-opus-mt-de-en", device=device)
+en_to_de = pipeline("translation", model="Helsinki-NLP-opus-mt-en-de", device=device)
 
 # Laden der Modelle französisch / deutsch und umgekehrt
-de_to_fr = pipeline("translation", model="Helsinki-NLP/opus-mt-de-fr", device=device)
-fr_to_de = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-de", device=device)
+de_to_fr = pipeline("translation", model="Helsinki-NLP-opus-mt-de-fr", device=device)
+fr_to_de = pipeline("translation", model="Helsinki-NLP-opus-mt-fr-de", device=device)
 
 # Übersetzungsfunktionen
 def translate_de_to_en(text):
