cd553db33bd32a427b3d5628eede5e6e6125205f
Markus Rößler <barytdruck@freenet.de>
2024-12-14 09:00:49 +0100

    Models

--- new.py
+++ new.py
@@ -12,12 +12,12 @@ device = torch.device('cpu')
 print(f"Verwendetes Gerät: {device}")
 
 # Laden der Modelle deutsch / englisch und umgekehrt
-de_to_en = pipeline("translation", model="Helsinki-NLP-opus-mt-de-en", device=device)
-en_to_de = pipeline("translation", model="Helsinki-NLP-opus-mt-en-de", device=device)
+de_to_en = pipeline("translation", model="Helsinki-NLP/opus-mt-de-en", device=device)
+en_to_de = pipeline("translation", model="Helsinki-NLP/opus-mt-en-de", device=device)
 
 # Laden der Modelle französisch / deutsch und umgekehrt
-de_to_fr = pipeline("translation", model="Helsinki-NLP-opus-mt-de-fr", device=device)
-fr_to_de = pipeline("translation", model="Helsinki-NLP-opus-mt-fr-de", device=device)
+de_to_fr = pipeline("translation", model="Helsinki-NLP/opus-mt-de-fr", device=device)
+fr_to_de = pipeline("translation", model="Helsinki-NLP/opus-mt-fr-de", device=device)
 
 
 class Uebersetzer(QWidget):
